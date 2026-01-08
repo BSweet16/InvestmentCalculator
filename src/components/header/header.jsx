782dae2afd7241601e6ffd240a42a7b9f60e65ca
Blake <blake.sweet16@gmail.com>
2026-01-08 15:22:07 -0500

    Intial project commit.
    Added header component already

---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import '/investment-calculator-logo.png';
+
+export function Header() {
+  return (
+    <header id="header">
+        <img src="/investment-calculator-logo.png" alt="Investment Calculator Logo" />
+        <h1>React Investment Calculator</h1>
+    </header>
+  );
+}
